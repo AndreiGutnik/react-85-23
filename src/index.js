0aa4e9fb16c499c1d256a648e421fd51866d49c6
Andrii Hutnyk <andreigutnik@gmail.com>
2023-07-30 13:29:53 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
+import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,30 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-
-// const data = paintings[0];
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <Painting
-//     url={data.url}
-//     title={data.title}
-//     authorUrl={data.author.url}
-//     authorTag={data.author.tag}
-//     price={data.price}
-//   />
-// );
-
-// const painting = (
-//   <div>
-//     <img src={data.url} alt={data.title} width="480" />
-//     <h2>{data.title}</h2>
-//     <p>
-// 			Автор: <a href={data.author.url}>{data.author.tag}</a>
-//     </p>
-// 		<p>Цена: {data.price}</p>
-//     <p>Доступность:</p>
-//     <button type="button">Добавить в корзину</button>
-//   </div>
-// );
-
-// ReactDOM.createRoot(document.getElementById('root')).render(painting);
